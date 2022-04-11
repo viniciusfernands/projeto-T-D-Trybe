391243ed35068942be4c5f9ca1b14ef6d3106370
viniciusfernands <viniciusfernandes126@gmail.com>
2022-04-11 15:45:32 -0300

    feat: override createdRacesInstances method and implement count new instance function for each created class

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,14 +2,24 @@ import Race from './Race';
 
 class Dwarf extends Race {
   private _maxLifePoints: number;
+  private static _dwarfInstanceCount = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 80;
+    Dwarf.addDwarfInstance();
   }
   
-  get maxLifePoints(): number {
+  public get maxLifePoints(): number {
     return this._maxLifePoints;
+  }
+
+  private static addDwarfInstance() {
+    this._dwarfInstanceCount += 1;
+  }
+
+  public static createdRacesInstances(): number {
+    return this._dwarfInstanceCount;
   }
 }
 
