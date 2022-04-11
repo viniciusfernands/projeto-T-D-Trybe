02df12e4131d754a67845fa158dc45b310e6afd8
viniciusfernands <viniciusfernandes126@gmail.com>
2022-04-11 15:27:13 -0300

    feat: create Elf class inherited from Race class

---
+++ src/Races/Elf.ts
@@ -0,0 +1,16 @@
+import Race from './index';
+
+class Elf extends Race {
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+  }
+  
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Elf;
