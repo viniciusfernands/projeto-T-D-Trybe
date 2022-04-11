63c8b9819ee9dec3d1f3b421740f744a65a896c9
viniciusfernands <viniciusfernandes126@gmail.com>
2022-04-11 15:48:40 -0300

    feat: override createdRacesInstances method and implement count new instance function for each created class

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -2,14 +2,24 @@ import Race from './Race';
 
 class Halfling extends Race {
   private _maxLifePoints: number;
+  private static _halflingInstanceCount = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 60;
+    Halfling.addHalflingInstance();
   }
   
   get maxLifePoints(): number {
     return this._maxLifePoints;
+  }
+
+  private static addHalflingInstance() {
+    this._halflingInstanceCount += 1;
+  }
+
+  public static createdRacesInstances(): number {
+    return this._halflingInstanceCount;
   }
 }
 
