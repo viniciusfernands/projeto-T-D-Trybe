b3831a49b656eee19bcfd33189a37f2fc89464e6
viniciusfernands <viniciusfernandes126@gmail.com>
2022-04-11 15:49:37 -0300

    feat: override createdRacesInstances method and implement count new instance function for each created class

--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -2,14 +2,24 @@ import Race from './Race';
 
 class Orc extends Race {
   private _maxLifePoints: number;
+  private static _orcInstanceCount = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 74;
+    Orc.addOrcInstance();
   }
   
   get maxLifePoints(): number {
     return this._maxLifePoints;
+  }
+
+  private static addOrcInstance() {
+    this._orcInstanceCount += 1;
+  }
+
+  public static createdRacesInstances(): number {
+    return this._orcInstanceCount;
   }
 }
 
