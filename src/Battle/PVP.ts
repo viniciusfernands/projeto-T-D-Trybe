98b1cfa74585a388d897069e45f8ed19bd45c0b4
viniciusfernands <viniciusfernandes126@gmail.com>
2022-04-12 17:03:08 -0300

    refactor: makes use of recursion to stop using for

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -7,15 +7,17 @@ export default class PVP extends Battle {
   }
 
   fight(): number {
-    for (let i = 0; i < 500; i += 1) {
-      this._player1.attack(this._player2);
-      this._player2.attack(this._player1);
+    this._player1.attack(this._player2);
+    this._player2.attack(this._player1);
+    
+    if (this._player1.lifePoints !== -1 && this._player2.lifePoints !== -1) {
+      return this.fight();
     }
-
+    
     if (this._player1.lifePoints === -1) {
-      return -1;
+      return -1; // -1 representa a vitoria do player2
     }
 
-    return 1;
+    return 1; // 1 representa a vitoria do player1
   }
 }
