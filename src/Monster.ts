3d720ce2b162ac110723c3855bfed7e1882bc235
viniciusfernands <viniciusfernandes126@gmail.com>
2022-04-12 15:09:40 -0300

    feat: create Monster class

---
+++ src/Monster.ts
@@ -0,0 +1,27 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  public get lifePoints() { return this._lifePoints; }
+
+  public get strength() { return this._strength; }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints > 0) { this._lifePoints -= attackPoints; }
+
+    if (this._lifePoints <= 0) { this._lifePoints = -1; }
+
+    return this.lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
